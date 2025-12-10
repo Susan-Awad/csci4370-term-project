6ea45f07fe022ee7edce010ffa28696440b7d91e
Susan-Awad <152205257+Susan-Awad@users.noreply.github.com>
2025-12-09 23:36:39 -0500

    added queries

--- queries.sql
+++ queries.sql
@@ -1,11 +1,23 @@
 
 -- Description: Registers a new user to the website
 -- URL: http://localhost:8081/register
-insert into user (username, password, firstName, lastName) values (?, ?, ?, ?)
+    insert into user (username, password, firstName, lastName) values (?, ?, ?, ?)
 
 -- Description: Verifies the logged in user 
 -- URL: http://localhost:8081/login
-select * from user where username = ?
+    select * from user where username = ?
+
+-- Descriptoin: The query updates the users profile information based on the input.
+-- URL: http://localhost:8081/profile
+    UPDATE user SET firstName = ?, lastName = ?, username = ? WHERE userId = ?
+
+-- Description: This query gets the username of the current user logged in.
+-- URL: http://localhost:8081/profile
+    SELECT username FROM user WHERE userId = ?
+
+-- Description: This query gets all the user information from the user id.
+-- URL:http://localhost:8081/profile
+    SELECT userId, firstName, lastName FROM user WHERE userId = ?
 
 -- Description: Retrieves all the users budgets they have created.
 -- URL: http://localhost:8081/budgets
@@ -37,7 +49,8 @@ select * from user where username = ?
     SET user_id = ?, category_id = ?, budget_month = ?, amount_budgeted = ? 
     WHERE budget_id = ?
     
--- Retrieve total income for a given month for a specific user
+-- Description: Retrieve total income for a given month for a specific user
+-- URL: http://localhost:8081/home
   SELECT SUM(transaction_amount)
   FROM transactions
   WHERE user_id = ?
@@ -49,7 +62,8 @@ select * from user where username = ?
     AND transaction_date >= ?
     AND transaction_date < ?;
 
--- Retrieve total expenses for a given month for a specific user
+-- Description: Retrieve total expenses for a given month for a specific user
+-- URL: http://localhost:8081/home
   SELECT SUM(transaction_amount)
   FROM transactions
   WHERE user_id = ?
@@ -61,7 +75,8 @@ select * from user where username = ?
   AND transaction_date >= ?
   AND transaction_date < ?;
 
--- Retrieve the N most recent transactions for a user
+-- Description: Retrieve the N most recent transactions for a user
+-- URL: http://localhost:8081/transactions
 SELECT t.transaction_id,
     t.transaction_date,
     t.transaction_amount,
@@ -75,7 +90,8 @@ WHERE t.user_id = ?
 ORDER BY t.transaction_date DESC, t.transaction_id DESC
 LIMIT ?;
 
--- Retrieve all transactions for a user
+-- Description: Retrieve all transactions for a user
+-- URL: http://localhost:8081/transactions
 SELECT t.transaction_id,
     t.transaction_date,
     t.transaction_amount,
@@ -88,12 +104,14 @@ JOIN categories c ON t.category_id = c.category_id
 WHERE t.user_id = ?
 ORDER BY t.transaction_date DESC, t.transaction_id DESC;
 
--- Insert a new transaction
+-- Description: Insert a new transaction
+-- URL: http://localhost:8081/transactions
 INSERT INTO transactions 
   (user_id, category_id, transaction_date, transaction_amount, transaction_description)
 VALUES (?, ?, ?, ?, ?);
 
--- Retrieve a single transaction by its ID
+-- Description: Retrieve a single transaction by its ID
+-- URL: http://localhost:8081/transactions
 SELECT t.transaction_id,
     t.transaction_date,
     t.transaction_amount,
@@ -105,7 +123,8 @@ FROM transactions t
 JOIN categories c ON t.category_id = c.category_id
 WHERE t.transaction_id = ?;
 
--- Update an existing transaction
+-- Description: Update an existing transaction
+-- URL: http://localhost:8081/transactions
 UPDATE transactions
 SET category_id = ?,
     transaction_date = ?,
@@ -113,6 +132,21 @@ SET category_id = ?,
     transaction_description = ?
 WHERE transaction_id = ?;
 
--- Delete a transaction by ID
+-- Description: Delete a transaction by ID
+-- URL: http://localhost:8081/transactions
 DELETE FROM transactions WHERE transaction_id = ?;
 
+-- Description: Adds a category to a specific users category list
+-- URL: http://localhost:8081/categories
+    INSERT INTO categories (category_name, category_type, category_color, user_id) VALUES (?, ?, ?, ?)
+
+-- Description: Deletes a category from the users category list
+-- URL: http://localhost:8081/categories
+    DELETE FROM categories WHERE category_id = ? AND user_id = ?
+
+-- Description: Gets all of the users cateories and information
+-- URL: http://localhost:8081/categories
+    SELECT category_id, category_name, category_type, category_color, user_id
+        FROM categories
+        WHERE user_id = ?
+        ORDER BY category_type, category_name
